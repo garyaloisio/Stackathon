a1a9d8dc0bc052faa54db2dd027116846693e632
Gary Aloisio <garyjaloisio@gmail.com>
2019-08-08 14:55:34 -0400

    my first commit

---
+++ components/Map.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, Stylesheet } from 'react-native';
+
+export function Map() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
